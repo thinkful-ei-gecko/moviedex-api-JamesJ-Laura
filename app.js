3230a15fef1214d4aa7c34e392047001e7c4b491
laura <laura.j.elias@gmail.com>
2019-09-24 14:40:32 -0500

    basic query parameters set up

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const morgan = require ('morgan');
 const cors = require ('cors');
-const moviedata = require ('./movies-data.js');
+const movieData = require ('./movies-data.js');
 require ('dotenv').config();
 
 const app = express();
@@ -10,7 +10,28 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200);
-  res.send(moviedata);
+  res.send(movieData);
+});
+
+app.get('/movie', (req, res) => {
+  const genre = req.query.genre ? req.query.genre.toLowerCase() : '';
+  const country = req.query.country ? req.query.country.toLowerCase() : '';
+  const avg_vote = req.query.avg_vote ? parseFloat(req.query.avg_vote) : '';
+
+  let filteredMovies = [...movieData];
+
+  if (genre){
+    filteredMovies = filteredMovies.filter(movie => movie.genre.toLowerCase().includes(genre));
+  }
+  if (country){
+    filteredMovies = filteredMovies.filter(movie => movie.country.toLowerCase().includes(country));
+  }
+  if (avg_vote){
+    filteredMovies = filteredMovies.filter(movie => movie.avg_vote >= avg_vote);
+  }
+  res.send(filteredMovies);
+
+
 });
 
 
