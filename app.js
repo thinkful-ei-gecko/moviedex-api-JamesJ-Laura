ed12d0311ee47807ec136a9b5e5a06d2cf5e4f06
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-24 16:17:53 -0400

    add helmet middleware, custom middleware for requiring api token from env

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require ('express');
 const morgan = require ('morgan');
 const cors = require ('cors');
+const helmet = require('helmet');
 const movieData = require ('./movies-data.js');
 require ('dotenv').config();
 
 const app = express();
 app.use(morgan('common'));
 app.use(cors());
+app.use(helmet());
+
+function authorize(req, res, next) {  
+  if(!req.headers.authorization) {
+    res.send('Bearer token is required!');
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  if(token !== process.env.API_KEY) {
+    res.send('Provided token is invalid!');
+  }
+  next();
+}
+app.use(authorize);
 
 app.get('/', (req, res) => {
   res.status(200);
   res.send(movieData);
 });
 
-app.get('/movie', (req, res) => {
+app.get('/movie', authorize, (req, res) => {
   const genre = req.query.genre ? req.query.genre.toLowerCase() : '';
   const country = req.query.country ? decodeURI(req.query.country).toLowerCase() : '';
   const avg_vote = req.query.avg_vote ? parseFloat(req.query.avg_vote) : '';
